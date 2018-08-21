81b9a19b0bb8256e66075cb2f9d74799ac58eafe
MrDaDaDo <42548841+MrDaDaDo@users.noreply.github.com>
2018-08-21 11:17:31 +0900

    commit

--- nHentai-Viewer.user.js
+++ nHentai-Viewer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         nHentai-Viewer
-// @version      2.0
+// @version      2.1
 // @author       MrDaDaDo
 // @include      /^https:\/\/nhentai\.net\/g\/(\d)+\/(\d)+\//
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
@@ -17,10 +17,10 @@
         var regexp = /data\-src="https:\/\/t\.nhentai\.net\/galleries\/(\d)+\/(\d)+t.[a-zA-Z]+"/g;
         var imageSrcs = galleryHtml.match(regexp);
         imageSrcs = imageSrcs.map( function(o){ return genImageUrl(o.replace('data-src=','').replace(/"/g,'')); });
-        imageSrcs.shift();
         return imageSrcs;
     };
     var showImage = function(imageSrcs) {
+        $container.html("");
         imageSrcs.forEach(function(imageSrc) {
             var imgDom = document.createElement('img');
             imgDom.src = imageSrc;
